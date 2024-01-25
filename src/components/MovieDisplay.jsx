fbe36298abccdc92774ce7ca8ba566589366d681
dharshan-m <dharshanmohan2704@gmail.com.com>
2024-01-25 14:34:20 +0530

    modified some errors in movieDispaly file and modified app name in index html file

--- src/components/MovieDisplay.jsx
+++ src/components/MovieDisplay.jsx
@@ -1,3 +1,180 @@
+import React, { useState, useEffect, useCallback } from 'react';
+import './MovieDisplay.css';
+import Navbar from './navbar';
+import { useNavigate } from 'react-router-dom';
+
+export default function MovieDisplay() {
+    const [displayMovie, setDisplayMovie] = useState([]);
+    const [category, setCategory] = useState('popular');
+
+    const navigate = useNavigate();
+
+    const clickDetailpage = (id) => {
+        navigate(`/movies/${id}`);
+    };
+
+    const fetchapi = useCallback(() => {
+        let url = '';
+
+    if (category === 'popular') {
+        url = 'https://api.themoviedb.org/3/discover/movie?api_key=f470f67e0a8d6712635048ad1c07ac53';
+    } else if (category === 'now_playing') {
+        url = 'https://api.themoviedb.org/3/movie/now_playing?api_key=f470f67e0a8d6712635048ad1c07ac53';
+    } else if (category === 'upcoming') {
+        url = 'https://api.themoviedb.org/3/movie/upcoming?api_key=f470f67e0a8d6712635048ad1c07ac53';
+    } else if (category === 'top_rated') {
+        url = 'https://api.themoviedb.org/3/movie/top_rated?api_key=f470f67e0a8d6712635048ad1c07ac53';
+    }
+
+    fetch(url)
+        .then((response) => response.json())
+        .then((data) => setDisplayMovie(data.results));
+    }, [category]);
+
+    useEffect(() => {
+        fetchapi();
+    }, [fetchapi, category]);
+
+    return (
+        <div className='moviedetails_navbar'>
+            <Navbar />
+            <div className='button_divs'>
+                <div className='div_front_content'>
+                    <h2 className='class_front_content'>Filter your movies by category</h2>
+                </div>
+                <div className='button_rows'>
+                    <button onClick={() => setCategory('popular')} className='subcategory_btn'>Popular</button>
+                    <button onClick={() => setCategory('now_playing')} className='subcategory_btn'>Now Playing</button>
+                    <button onClick={() => setCategory('top_rated')} className='subcategory_btn'>Top Rated</button>
+                    <button onClick={() => setCategory('upcoming')} className='subcategory_btn'>Upcoming</button>
+                </div>
+            </div>
+            <div className='entire_moviedetails'>
+                {displayMovie.map((movies) => (
+                    <div key={movies.id} className='mevieDispaly'>
+                        <img src={`https://image.tmdb.org/t/p/w500${movies.poster_path}`} alt={movies.title} className='movies_imageDisply' onClick={() => clickDetailpage(movies.id)}/>
+                        <h3 className='movieDispaly_title'>{movies.title}</h3>
+                        <p className='movieDispaly_releasedate'>{movies.release_date}</p>
+                    </div>
+                ))}
+            </div>
+        </div>
+    );
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+// class MovieDisplay extends Component
+// export default function MovieDisplay()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 // import React, {useState, useEffect} from 'react'
 // import './MovieDisplay.css'
 // import Navbar from './navbar';
@@ -77,72 +254,3 @@
 //         </div>
 //     );
 // }
-
-
-
-
-
-
-import React, { useState, useEffect } from 'react';
-import './MovieDisplay.css';
-import Navbar from './navbar';
-import { useNavigate } from 'react-router-dom';
-
-export default function MovieDisplay() {
-    const [displayMovie, setDisplayMovie] = useState([]);
-    const [category, setCategory] = useState('popular');
-
-    useEffect(() => {
-        fetchapi();
-    }, [category]);
-
-    const navigate = useNavigate();
-
-    const clickDetailpage = (id) => {
-        navigate(`/movies/${id}`);
-    };
-
-    const fetchapi = () => {
-        let url = '';
-
-    if (category === 'popular') {
-        url = 'https://api.themoviedb.org/3/discover/movie?api_key=f470f67e0a8d6712635048ad1c07ac53';
-    } else if (category === 'now_playing') {
-        url = 'https://api.themoviedb.org/3/movie/now_playing?api_key=f470f67e0a8d6712635048ad1c07ac53';
-    } else if (category === 'upcoming') {
-        url = 'https://api.themoviedb.org/3/movie/upcoming?api_key=f470f67e0a8d6712635048ad1c07ac53';
-    } else if (category === 'top_rated') {
-        url = 'https://api.themoviedb.org/3/movie/top_rated?api_key=f470f67e0a8d6712635048ad1c07ac53';
-    }
-
-    fetch(url)
-        .then((response) => response.json())
-        .then((data) => setDisplayMovie(data.results));
-};
-
-    return (
-        <div className='moviedetails_navbar'>
-            <Navbar />
-            <div className='button_divs'>
-                <div className='div_front_content'>
-                    <h2 className='class_front_content'>Filter your movies by category</h2>
-                </div>
-                <div className='button_rows'>
-                    <button onClick={() => setCategory('popular')} className='subcategory_btn'>Popular</button>
-                    <button onClick={() => setCategory('now_playing')} className='subcategory_btn'>Now Playing</button>
-                    <button onClick={() => setCategory('top_rated')} className='subcategory_btn'>Top Rated</button>
-                    <button onClick={() => setCategory('upcoming')} className='subcategory_btn'>Upcoming</button>
-                </div>
-            </div>
-            <div className='entire_moviedetails'>
-                {displayMovie.map((movies) => (
-                    <div key={movies.id} className='mevieDispaly'>
-                        <img src={`https://image.tmdb.org/t/p/w500${movies.poster_path}`} alt={movies.title} className='movies_imageDisply' onClick={() => clickDetailpage(movies.id)}/>
-                        <h3 className='movieDispaly_title'>{movies.title}</h3>
-                        <p className='movieDispaly_releasedate'>{movies.release_date}</p>
-                    </div>
-                ))}
-            </div>
-        </div>
-    );
-}
